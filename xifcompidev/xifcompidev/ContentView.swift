a0de7e49403bb69c2cb526276998a233b3ed650e
Otto Schmilinsky <otto_st@hotmail.com>
2021-12-26 11:08:47 -0500

    Project Structure, First Commit
    This commit start with the project structure.

---
+++ xifcompidev/xifcompidev/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  xifcompidev
+//
+//  Created by Otto Schmilinsky T. on 26/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		ZStack {
+			Text("First Commit! - Project Structure -")
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xifcompidev/xifcompidev/xifcompidevApp.swift
@@ -0,0 +1,17 @@
+//
+//  xifcompidevApp.swift
+//  xifcompidev
+//
+//  Created by Otto Schmilinsky T. on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct xifcompidevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
